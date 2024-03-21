4eaad890e0fd7d0f1836538ec23ae7d292a72007
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-21 12:10:27 -0300

    Exercise solution

--- src/main/java/LogLevel.java
+++ src/main/java/LogLevel.java
@@ -1,3 +1,11 @@
 public enum LogLevel {
-    // TODO: define members for each log level
+    TRACE(1), DEBUG(2), INFO(4), WARNING(5), ERROR(6), FATAL(42), UNKNOWN(0);
+
+    public final int value;
+
+    LogLevel(int value) {
+        this.value = value;
+    }
+
+
 }
--- src/main/java/LogLine.java
+++ src/main/java/LogLine.java
@@ -1,13 +1,28 @@
 public class LogLine {
 
+    private final LogLevel logLevel;
+    private final String message;
+
     public LogLine(String logLine) {
+        var splitIndex = logLine.indexOf(":");
+        var shortLevel = logLine.substring(1, splitIndex - 1).trim().toUpperCase();
+        logLevel = switch (shortLevel) {
+            case "TRC" -> LogLevel.TRACE;
+            case "DBG" -> LogLevel.DEBUG;
+            case "INF" -> LogLevel.INFO;
+            case "WRN" -> LogLevel.WARNING;
+            case "ERR" -> LogLevel.ERROR;
+            case "FTL" -> LogLevel.FATAL;
+            default -> LogLevel.UNKNOWN;
+        };
+        message = logLine.substring(splitIndex + 1).trim();
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+        return logLevel;
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        return String.format("%d:%s", logLevel.value, message);
     }
 }
